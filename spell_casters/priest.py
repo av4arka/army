4533f317929c747666703ffd6fd0057d5950f35d
av4arka <tima.larionoww@gmail.com>
2018-11-06 17:26:16 +0200

    import and using of class PriestAbilitiy in class Priest

--- spell_casters/priest.py
+++ spell_casters/priest.py
@@ -1,4 +1,5 @@
 from spell_casters.spell_caster import SpellCaster
+from abilities.priest_abilitiy import PriestAbilitiy
 from spells.heal import Heal
 
 class Priest(SpellCaster):
@@ -6,3 +7,7 @@ class Priest(SpellCaster):
 
     def __init__(self, title='Priest', hit_points_limite=95, damage=17, mana_limit=85):
         SpellCaster.__init__(self, title, hit_points_limite, damage, mana_limit, Heal())
+        self._priest_abilitiy = PriestAbilitiy(self)
+
+    def attack(self, enemy):
+        self._priest_abilitiy.attack(enemy)
