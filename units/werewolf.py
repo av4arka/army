656dccd59c2d666e9cc22ae610848efa1708b6c2
av4arka <tima.larionoww@gmail.com>
2018-11-06 10:29:54 +0200

    add child class Werewolf

---
+++ units/werewolf.py
@@ -0,0 +1,20 @@
+from units.unit import Unit
+from abilities.werewolf_abilitiy import WerewolfAbilitiy
+from states.werewolf_state import WerewolfState
+
+class Werewolf(Unit):
+
+
+    def __init__(self, title, hit_points_limit, damage):
+        Unit.__init__(self, title, hit_points_limit, damage)
+        self._werewolf_state = WerewolfState(title, hit_points_limit, damage, self)
+        self._werewolf_abilitiy = WerewolfAbilitiy(self, self._werewolf_state)
+
+    def use_abilitiy_one(self, target):
+        self._werewolf_abilitiy.use_abilitiy_one(target)
+
+    def use_abilitiy_two(self):
+        self._werewolf_abilitiy.use_abilitiy_two()
+
+    def take_magic_damage(self, damage):
+        self._werewolf_state.take_magic_damage(damage)
