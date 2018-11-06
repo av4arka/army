a11d589ca9f5060b64c1fa9df884b4aa3442ca17
av4arka <tima.larionoww@gmail.com>
2018-11-06 12:57:33 +0200

    import class MagicAbilitiy and Lightning in SpellCaster

--- spell_casters/spell_caster.py
+++ spell_casters/spell_caster.py
@@ -1,5 +1,7 @@
 from units.unit import Unit
 from states.magic_state import MagicState
+from abilities.magic_abilitiy import MagicAbilitiy
+from spells.lightning import Lightning
 
 class SpellCaster(Unit):
 
@@ -7,6 +9,8 @@ class SpellCaster(Unit):
     def __init__(self, title, hit_points_limit, damage, mana_limit):
         Unit.__init__(self, title, hit_points_limit, damage)
         self._magic_state = MagicState(title, hit_points_limit, damage, mana_limit)
+        self._spell = Lightning(10, 10)
+        self._magic_abilitiy = MagicAbilitiy(self, self._spell)
 
     @property
     def mana(self):
@@ -15,6 +19,13 @@ class SpellCaster(Unit):
     @property
     def mana_limit(self):
         return self._magic_state.mana_limit
+
+    @property
+    def spell(self):
+        return self._spell
+
+    def use_abilitiy_one(self, target):
+        self._magic_abilitiy.use_abilitiy_one(target)
 
     def expend_mana(self, amount_mana):
         self._magic_state.expend_mana(amount_mana)
