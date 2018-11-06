181e75a625a493127d171ddff95f1dbe947ca8cb
av4arka <tima.larionoww@gmail.com>
2018-11-06 17:20:49 +0200

    add class PriestAbilitiy

---
+++ abilities/priest_abilitiy.py
@@ -0,0 +1,19 @@
+from abilities.abilitiy import Abilitiy
+
+class PriestAbilitiy(Abilitiy):
+
+
+    def __init__(self, attacker):
+        Abilitiy.__init__(self, attacker)
+        self._attacker = attacker
+
+    def attack(self, enemy):
+        damage_attacker = self._attacker.damage
+        self._attacker.ensure_is_alive()
+
+        if self._attacker.title == 'Priest' and enemy.is_undead:
+            damage_attacker *= 2
+
+        enemy.take_damage(damage_attacker)
+        if enemy.hit_points > 0:
+            self.counter_attack(enemy)
