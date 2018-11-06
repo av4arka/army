4553d81576876230217c99dbac333fc21ce1c438
av4arka <tima.larionoww@gmail.com>
2018-11-06 20:37:37 +0200

    add method valid_arguments

--- units/unit.py
+++ units/unit.py
@@ -5,12 +5,17 @@ from states.default_state import DefaultState
 class Unit:
 
     def __init__(self, title, hit_points_limit, damage):
-        if type(hit_points_limit) != int or type(damage) != int or type(title) != str:
+        if not self.valid_arguments(title, hit_points_limit, damage):
             raise ValueError
 
         self._default_state = DefaultState(title, hit_points_limit, damage, self)
         self._abilitiy = Abilitiy(self)
 
+    def  valid_arguments(self, title, hit_points_limit, damage):
+        if isinstance(hit_points_limit, int) and isinstance(damage, int) \
+                and isinstance(title, str):
+            return True
+        return False
 
     def ensure_is_alive(self):
         return self._default_state.ensure_is_alive()
