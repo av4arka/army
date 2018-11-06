2a8e54602206d22b75ec6eb52873991b5150b740
av4arka <tima.larionoww@gmail.com>
2018-11-06 20:15:38 +0200

    add aggregate-class Demon

---
+++ units/demon.py
@@ -0,0 +1,31 @@
+from units.unit import Unit
+from exception.exception_unit import UnitIsDead
+
+class Demon(Unit):
+
+
+    def __init__(self, warlock, title='Demon', hit_points_limit=80, damage=30):
+        Unit.__init__(self, title, hit_points_limit, damage)
+        self._warlock = warlock
+
+    def ensure_is_alive(self):
+        if self.hit_points <= 0:
+            self.hit_points = 0
+            self._warlock.demon_is_dead(self)
+            raise UnitIsDead(self.title + ' is dead!')
+
+
+    # def ensure_is_alive(self):
+    #     void
+    #     Demon::ensureIsAlive()
+    #     {
+    #     if (getHitPoints() <= 0)
+    #     {
+    #
+    #         setHitPoints(0);
+    #     warlock->eraseDemon(this);
+    #     notify();
+    #     throw
+    #     UnitIsDead();
+    #     }
+    #     }
