0c051d17a7cc22f0c7a4e55da49114262ceefcf2
av4arka <tima.larionoww@gmail.com>
2018-11-07 17:31:47 +0200

    import files from project army

--- my_army/my_army.py
+++ my_army/my_army.py
@@ -1,1 +1,12 @@
+from units.soldier import Soldier
+from units.berserker import Berserker
+from units.roque import Roque
+from units.vampire import Vampire
+from units.werewolf import Werewolf
+from units.demon import Demon
+from spell_casters.wizard import Wizard
+from spell_casters.healer import Heal
+from spell_casters.priest import Priest
+from spell_casters.warlock import Warlock
+from spell_casters.necromancer import Necromancer
 
